c1f6d56d9866f1031e75c195d0d4119b67aea098
Prashant Gupta <145412708+prashant-g0@users.noreply.github.com>
2024-09-25 22:36:16 +0530

    Update kadane'sAlgorithm.py

--- kadane'sAlgorithm.py
+++ kadane'sAlgorithm.py
@@ -1,21 +1,6 @@
-#The idea is to store the maximum sum of contiguous subarray.
-
-#For example:
-#array = [-2,-3,4,-1,-2,1,5,-3]
-#For this given array the subarray(s) will be [-2], [-2,-3,4], [-3,4,-1].....
-#Subarray(s) are smaller parts of original array.
-
-#Contiguous memory refers to a block of memory addresses that are sequentially 
-# next to each other, without any gaps. This means that the data is stored in adjacent memory locations.
-
-#For our example array the subarray with maximu sum is [4,-1,-2,1,5]
-#For which the sum is = 7
-
-#Time complexity: O(n), Auxiliary space: O(1)
-
 #Code:
 
-#Taking array from user
+#Example array, you can  take array from user also
 arr = [-2,-3,4,-1,-2,1,5,-3]
 print("Array: ",arr)
 
@@ -34,3 +19,21 @@ for i in range(len(arr)):
         totalSum = 0
 #print the output
 print("Subarray with max sum is: ",max_num)
+
+#Explaination:
+#The idea is to store the maximum sum of contiguous subarray.
+
+#For example:
+#array = [-2,-3,4,-1,-2,1,5,-3]
+#For this given array the subarray(s) will be [-2], [-2,-3,4], [-3,4,-1].....
+#Subarray(s) are smaller parts of original array.
+
+#Contiguous memory refers to a block of memory addresses that are sequentially 
+# next to each other, without any gaps. This means that the data is stored in adjacent memory locations.
+
+#For array the subarray with maximum sum is [4,-1,-2,1,5]
+#For which the sum is = 7
+
+#Time complexity: O(n), Auxiliary space: O(1)
+
+
