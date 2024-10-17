8115ed5ff8138f2e158fa5a8221b9e388c0ab6b9
Prashant Gupta <145412708+prashant-g0@users.noreply.github.com>
2024-10-17 23:34:54 +0530

    Update jumpSearch.py

--- searchingAlgorithms/jumpSearch.py
+++ searchingAlgorithms/jumpSearch.py
@@ -9,3 +9,30 @@ This method strikes a balance between linear search and binary search for effici
 Time complexity: O(sqrt{n})
 Auxiliary space: O(1)
 """
+
+import math
+
+def jump_search(arr, target):
+    n = len(arr)
+    step = int(math.sqrt(n))  # Step size
+    
+    # Finding the block where element is present (if it is present)
+    prev = 0
+    while arr[min(step, n) - 1] < target:
+        prev = step
+        step += int(math.sqrt(n))
+        if prev >= n:
+            return -1
+    
+    # Doing a linear search for target in the block beginning with prev
+    for i in range(prev, min(step, n)):
+        if arr[i] == target:
+            return i+1
+    return -1
+
+# Example usage
+arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+target = -1
+result = jump_search(arr, target)
+if result == -1: print("Element not found")
+else: print(f"Element found at index {result}")
